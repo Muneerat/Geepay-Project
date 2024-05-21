dbe8b0cdf7553802deb9ba2f19f32c2ed8bb54fb
Muneerat <oladipomunirat@gmail.com>
2024-05-21 11:20:27 +0100

    Update the sidebar change some icons

--- src/Component/SideBar.jsx
+++ src/Component/SideBar.jsx
@@ -1,54 +1,48 @@
 import React from 'react'
-import Logo, { Cube, Dark, DotBox, Important, Light, Logout, Person, Rectangle, Setting, SideIn, Star } from '../assets/icon'
+import Logo, { Cube, Dark, DotBox, Light, Logout, Person, Setting, SideIn, Star } from '../assets/icon'
 import { Link } from '@nextui-org/react'
 import useDarkMode from 'use-dark-mode'
 
 export default function SideBar() {
   const darkMode = useDarkMode(false)
   return (
-    <div className='flex flex-col bg-[#f7f8fa] w-[80px] items-center border h-screen justify-between '>
+    <div className='flex flex-col bg-[#fff] w-[94px] items-center border h-screen justify-between rounded-ful rounded-l-3xl'>
     <div className='flex flex-col'>
-    <Link to="#" className='py-5 transition-all hover:scale-125 duration-200 '>
-      <Logo width={40} height={40} />
+    <Link to="#" className='py-6 transition-all hover:scale-125 duration-200 '>
+      <Logo width={42} height={42} />
     </Link>
     <Link to="#" className='py-4 transition-all hover:scale-125 duration-200 '>
-      <DotBox width={28} height={28} />
+      <DotBox width={30} height={30} />
     </Link>
     <Link to="#" className='py-4 transition-all hover:scale-125 duration-200 '>
-      <Rectangle width={28} height={28} />
+      <Person width={30} height={30} />
     </Link>
     <Link to="#" className='py-4 transition-all hover:scale-125 duration-200 '>
-      <Person width={28} height={28} />
-    </Link>
-    <Link to="#" className='py-4 transition-all hover:scale-125 duration-200 '>
-      <Cube width={28} height={28} />
+      <Cube width={30} height={30} />
     </Link>  
     <Link to="#" className='py-4 transition-all hover:scale-125 duration-200 '>
-      <Star width={28} height={28} />
-    </Link>
-    <Link to="#" className='py-4 transition-all hover:scale-125 duration-200 '>
-      <Important width={28} height={28} />
+      <Star width={30} height={30} />
     </Link>
     <Link to="#" className='py-4 transition-all hover:scale-125 duration-200 '>
     <button onClick={darkMode.disable}>
-      <Light width={28} height={28} />
+      <Light width={30} height={30} />
     </button>
     </Link>
     <Link to="#" className='py-4 transition-all hover:scale-125 duration-200 '>
     <button onClick={darkMode.enable}>
-      <Dark width={28} height={28} />
+      <Dark width={30} height={30} />
     </button>
     </Link>
     </div>
     <div className='flex flex-col pb-10'>
     <Link to="#" className='py-4 transition-all hover:scale-125 duration-200 '>
-      <SideIn width={28} height={28} />
+      <SideIn width={30} height={30} />
     </Link>
     <Link to="#" className='py-4 transition-all hover:scale-125 duration-200 '>
-      <Setting width={28} height={28} />
+      <Setting width={30} height={30} />
     </Link>
     <Link to="#" className='py-4 transition-all hover:scale-125 duration-200 '>
-      <Logout width={28} height={28} />
+      <Logout width={30} height={30} />
     </Link>
     </div>
     </div>
