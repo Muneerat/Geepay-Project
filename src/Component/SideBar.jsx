9e0b9be72d71257e45612ade9e2f62f2147c6246
Muneerat <oladipomunirat@gmail.com>
2024-07-05 14:37:35 +0100

    Improve the navbar

--- src/Component/SideBar.jsx
+++ src/Component/SideBar.jsx
@@ -16,7 +16,7 @@ import LogoutModal from "./LoginOutModal";
 
 export default function SideBar() {
   // const darkMode = useDarkMode(false);
-  const [showSidebar, setShowSidebar] = useState(false);
+  const [showSidebar, setShowSidebar] = useState(true);
 
   const toggleSidebar = () => {
     setShowSidebar(!showSidebar);
@@ -24,10 +24,10 @@ export default function SideBar() {
   return (
     <div className="h-screen relative">
       <div
-        className={`flex-col bg-[#fff] w-[94px] items-center border h-screen justify-between rounded-ful rounded-l-3xl ${showSidebar ? ' hidden ' : 'md:flex '} `}
+        className={` md:flex-col bg-[#fff] md:w-[94px] w-[42px]  items-center border h-screen justify-between md:static absolute rounded-l-3xl z-30 ${showSidebar ? '  hidden' : 'md:flex '} `}
         onClick={toggleSidebar}
       >
-        <div className="flex flex-col justify-center ">
+        <div className="flex flex-col justify-center items-center">
           <Link
             to="#"
             className="py-6 transition-all hover:scale-125 duration-200 cursor-pointer "
@@ -102,7 +102,7 @@ export default function SideBar() {
         </div>
       </div>
       
-        <div className={`${showSidebar ? 'flex' : 'md:hidden flex'}  absolute cursor-pointer -left-1 top-12 animate-pulse hover:animate-none  `}>
+        <div className={`${showSidebar ? 'flex' : 'md:hidden flex'} z-10   absolute cursor-pointer -left-1 top-12 animate-pulse hover:animate-none  `}>
           <Link
             to="#"
             className="py-6 px-2 transition-all hover:scale-110 duration-200"
@@ -114,5 +114,102 @@ export default function SideBar() {
         </div>
      
     </div>
+  //   <div className="h-screen relative">
+  //   {/* Sidebar */}
+  //   <div
+  //     className={`md:flex-col bg-[#fff] w-[94px] items-center border h-screen justify-between rounded-full rounded-l-3xl transition-transform duration-300 ${
+  //       showSidebar ? 'translate-x-0' : '-translate-x-full'
+  //     }`}
+  //   >
+  //     <div className="flex flex-col justify-center items-center">
+  //       <Link
+  //         to="#"
+  //         className="py-6 transition-all hover:scale-125 duration-200 cursor-pointer"
+  //         onClick={toggleSidebar}
+  //       >
+  //         <Logo width={42} height={42} />
+  //       </Link>
+  //       <Link
+  //         to="#"
+  //         className="py-4 transition-all hover:scale-125 duration-200"
+  //       >
+  //         <DotBox width={30} height={30} />
+  //       </Link>
+  //       <Link
+  //         to="#"
+  //         className="py-4 transition-all hover:scale-125 duration-200"
+  //       >
+  //         <Person width={30} height={30} />
+  //       </Link>
+  //       <Link
+  //         to="#"
+  //         className="py-4 transition-all hover:scale-125 duration-200"
+  //       >
+  //         <Cube width={30} height={30} />
+  //       </Link>
+  //       <Link
+  //         to="#"
+  //         className="py-4 transition-all hover:scale-125 duration-200"
+  //       >
+  //         <Star width={30} height={30} />
+  //       </Link>
+  //       <Link
+  //         to="#"
+  //         className="py-4 transition-all hover:scale-125 duration-200"
+  //       >
+  //         {/* onClick={darkMode.disable} */}
+  //         <button>
+  //           <Light width={30} height={30} />
+  //         </button>
+  //       </Link>
+  //       <Link
+  //         to="#"
+  //         className="py-4 transition-all hover:scale-125 duration-200"
+  //       >
+  //         {/* onClick={darkMode.enable} */}
+  //         <button>
+  //           <Dark width={30} height={30} />
+  //         </button>
+  //       </Link>
+  //     </div>
+  //     <div className="flex flex-col pb-10 justify-center items-center">
+  //       <Link
+  //         to="#"
+  //         className="py-4 transition-all hover:scale-125 duration-200"
+  //         onClick={toggleSidebar}
+  //       >
+  //         <SideIn width={30} height={30} />
+  //       </Link>
+  //       <Link
+  //         to="#"
+  //         className="py-4 transition-all hover:scale-125 duration-200"
+  //       >
+  //         <Setting width={30} height={30} />
+  //       </Link>
+  //       <Link
+  //         to="#"
+  //         className="py-4 transition-all hover:scale-125 duration-200"
+  //       >
+  //         <LogoutModal />
+  //       </Link>
+  //     </div>
+  //   </div>
+
+  //   {/* Logo to toggle sidebar in mobile view */}
+  //   <div
+  //     className={`${
+  //       showSidebar ? 'hidden' : 'flex'
+  //     } absolute cursor-pointer -left-1 top-12 animate-pulse hover:animate-none`}
+  //   >
+  //     <Link
+  //       to="#"
+  //       className="py-6 px-2 transition-all hover:scale-110 duration-200"
+  //       onClick={toggleSidebar}
+  //     >
+  //       <Logo width={38} height={38} />
+  //     </Link>
+  //   </div>
+  // </div>
+
   );
 }
